ae9bd279191b861a3d2dac312ffc59f4970e429a
Kartar-Singh714 <kartarsingh7993@gmail.com>
2025-06-09 17:10:38 +0530

    Adding Practice Programs

---
+++ Program32_Count_Without_Len_String.py
@@ -0,0 +1,11 @@
+st = input("Enter the String: ")
+l=0
+l1=0
+for i in st:
+    l+=1
+    if i == " ":
+        st.replace(" ", "")
+        l-=1
+    l1+=1
+print(f"The length of the string is {l}")
+print(f"The length of the string without whitespaces is {l1}")
